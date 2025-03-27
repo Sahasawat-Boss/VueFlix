6cc69301285f5b2a79c5f0ec49b30ec706f50af9
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-27 21:32:31 +0700

    Added Movies Data and Comps MovieDetails

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,31 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import Side from "@/components/Side.vue"
+import MovieDetails from '@/components/MovieDetails.vue';
 
-import Magnify from 'vue-material-design-icons/Magnify.vue';
-import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
-import TrendingUp from 'vue-material-design-icons/TrendingUp.vue';
-import Television from 'vue-material-design-icons/Television.vue';
-import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
-import Plus from 'vue-material-design-icons/Plus.vue';
-import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
-
-import { useMovieStore } from './stores/movie'
+import { onMounted } from "vue";
+import movies from "./data/movies.json";
+import { useMovieStore } from './stores/movie';
 import { storeToRefs } from 'pinia';
-const useMovie = useMovieStore()
-const { movie, showFullVideo } = storeToRefs(useMovie)
+
+const useMovie = useMovieStore();
+const { movie, showFullVideo } = storeToRefs(useMovie);
+
+onMounted(() => {
+  setTimeout(() => movie.value = movies[0][0], 100);
+});
 </script>
 
 <template>
-  <div class="fixed w-full h-screen bg-black">
-    <div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
-      <div>
-        <div class="py-2 mx-10 my-6">
-          <Magnify fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
-        </div>
-        <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
-          <HomeOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
-        </div>
-        <div class="py-2 mx-10 my-6">
-          <TrendingUp fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
-        </div>
-        <div class="py-2 mx-10 my-6">
-          <Television fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
-        </div>
-        <div class="py-2 mx-10 my-6">
-          <MovieOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
-        </div>
-        <div class="py-2 mx-10 my-6">
-          <Plus fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
-        </div>
+  <main class="fixed w-full h-screen bg-black">
+    <Side />
+
+    <div v-if="!showFullVideo">
+      <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
+        <div class="absolute z-30 h-[600px] left-[120px] w-[77%] right-0 top-0 bg-gradient-to-r from-black via-black" />
+        <MovieDetails v-if="movie" :movie="movie" />
+        <video v-if="movie" :src="'/videos/' + movie.name + '.mp4'" autoplay muted playsinline loop
+          class="absolute z-0 h-[600px] right-0 top-0" />
       </div>
     </div>
-  </div>
+  </main>
 </template>
-
-
-<style scoped></style>
