26b3fd401381fec976e94e885759c010cf62f723
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-28 22:26:21 +0700

    Update Hero VDO

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,15 +1,15 @@
 <template>
     <nav class="flex items-center justify-between top-0 left-0 w-full z-[99] bg-black shadow-xl px-6 py-1.5 md:py-2">
         <!-- Logo -->
-        <router-link to="/" class="gradient-text-green text-3xl md:text-4xl font-bold hover110">
+        <router-link to="/" class="gradient-text-green text-3xl md:text-4xl font-bold hover110 fade-in-left">
             VF 
-            <span class="text-sm">[Demo]</span>
+            <span class="text-sm ">[Demo]</span>
         </router-link>
         
         <!-- To https://bossbsynth.com/-->
         <div class="flex items-center">
             <a href="https://bossbsynth.com/" target="_blank" rel="noopener noreferrer"
-                class="gradient-text-green text-xs font-semibold hover110">
+                class="gradient-text-green text-xs font-semibold hover110 fade-in-right">
                 bossbsynth.com
             </a>
         </div>
